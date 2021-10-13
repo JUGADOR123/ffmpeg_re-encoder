31dd3baac40d5f4100f823ca2d10e778605c49d8
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-10-13 12:32:10 -0600

    initial data classes and update of the outline

---
+++ src/dataclass.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from typing import List
+import shutil
+
+
+@dataclass
+class statistics:
+    """A class that holds all the statistic based variables"""
+
+    total_files: int
+    total_files_renamed: int
+    total_failed_renamed_files: int
+    total_probed_files: int
+    total_failed_probed_files: int
+    total_number_of_files_to_encode: int
+    remaining_files_to_encode: int
+    total_number_of_encoded_files: int
+    total_number_of_failed_encodes: int
+    starting_time: float
+    ending_time: float
+    individual_encoding_starting_time: float
+    individual_encoding_ending_time: float
+    initial_file_size: float
+    ending_file_size: float
+    space_saved: float
+    percentage_saved: float
+
+    average_time: float = round(
+        individual_encoding_ending_time - individual_encoding_ending_time, 3
+    )
+    time_remaining: float = round(
+        (remaining_files_to_encode * average_time) - starting_time, 3
+    )
+
+
+@dataclass
+class variables:
+    """A class that holds all useful variables"""
+
+    input_file_path: str
+    output_file_path: str
+    files_to_encode: List[tuple]
+    hwacc: bool = False
+
+    width = shutil.get_terminal_size((80, 20)).columns
+    HEADER = "\033[95m"
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    ORANGE = "\033[93m"
+    RED = "\033[91m"
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
