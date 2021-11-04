a3e0751919339725699a181dca694156f6ae496f
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2021-11-04 14:31:51 -0600

    additions
    file with possible ffmpeg commands
    initial iteration through encoding list

--- src/encoding.py
+++ src/encoding.py
@@ -1,3 +1,4 @@
+import subprocess
 from time import sleep
 from src.dataclass import variables as v
 from src.dataclass import statistics as stats
@@ -6,8 +7,21 @@ from src.dataclass import colors as c
 class Encoder:
     def __init__(self) -> None:
         pass
+    def _iterate(self) -> None:
+        if v.video_tracks_to_encode:
+            for path in v.video_tracks_to_encode:
+                self._encoding(path)
+    def _encoding(self,path:str) -> None:
+        try:
+            p=subprocess.Popen(f'ffmpeg -v quiet -i "{path}" -map 0 -c copy -c:v hevc_nvenc -preset p7 -rc cbr')
+        except Exception as e:
+            pass
+
     def _info(self)-> None:
-        pass
+        print(f"{c.RED}{c.BOLD}{'='*c.width}{c.RESET}")
+        print(f"{c.GREEN}{c.BOLD}Encoding will start now{c.RESET}")
+        print(f"{c.RED}{c.BOLD}{'='*c.width}{c.RESET}")
     def run(self) -> None:
         self._info()
         sleep(5)
+        self._iterate()
